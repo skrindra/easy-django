2d82d84f115180bcf08d74486d98de430170c033
Shiva <122610403+skrindra@users.noreply.github.com>
2025-05-03 18:04:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="go-django",
-    version="0.1.0",
+    version="0.0.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
